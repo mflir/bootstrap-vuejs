63a774d5750ca07ec398cbaac2da9bceb0c1e887
Florencia Scavone <scavone_f@hotmail.com>
2018-03-05 16:57:18 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+new Vue({
+	el: '#appVue',
+	data:{
+		lists:[
+			{keep:'uno',completed:false},
+			{keep:'dos', completed: false},
+			{keep:'tres', completed: true},
+
+		],
+
+   		newKeep:''
+	},
+	methods:{
+		addKeep:function(){
+			this.lists.push({keep: this.newKeep, completed: false});
+			this.newKeep = '';
+		}
+	}
+}) ;
